8699028a612e7b833c27145b6a63fa461fc4fb0e
Andrés Hernández <ingeingero@gmail.com>
2018-03-18 22:50:04 -0500

    Actualización documentación.
    Actualización documentación.
    
    Co-Authored-By: gitahernandez <gitahernandez@users.noreply.github.com>

--- ClothesFactoryStore/src/clothesFactoryStore/Rest/ClothesFactoryStoreWebServices.java
+++ ClothesFactoryStore/src/clothesFactoryStore/Rest/ClothesFactoryStoreWebServices.java
@@ -98,7 +98,7 @@ public class ClothesFactoryStoreWebServices {
 	}
 	
 	@Path("/getCiudadesEnvio")
-	@GET
+	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public ArrayList<CiudadesEnvioDTO> getCiudadesEnvio() {
